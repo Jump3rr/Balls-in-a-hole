2d9a746685da303fb41e0faae48ff60057f00555
Jump3rr <theneonxd@gmail.com>
2020-11-13 02:20:26 +0100

    stworzenie piłki poruszającej się zgodnie z ruchem telefonu

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
+const ballOnPage = document.querySelector('.ball');
+const board = document.querySelector('.board');
+const maxWidth = board.clientWidth - ballOnPage.clientWidth;
+const maxHeight = board.clientHeight - ballOnPage.clientHeight;
+
 window.addEventListener('deviceorientation', onDeviceMove)
+
 const ball = {
     x: 0,
     y: 0
 }
-function gameinit() {
-    const ballStartXPos = Math.random()*1000;
-    ball.x = ballStartXPos;
-}
+console.log(maxHeight);
+console.log(maxWidth);
+/*function gameinit() {
+    const ballStartXPos = Math.random()*99;
+    ballOnPage.style.left = ballStartXPos + "%";
+    const ballStartYPos = Math.random()*99;
+    ballOnPage.style.top = ballStartYPos + "%";
+}*/
 function onDeviceMove(ev) {
-    console.log(ev.alpha, ev.beta, ev.gamma);
+    ball.x =ev.alpha;
+    ball.y =ev.beta;
+
+    if(ball.x>90) {ball.x = 90};
+    if(ball.x<-90) {ball.x=-90};
+    if(ball.y>179 || ball.y<-90) {ball.y = 179};
+    if(ball.y<0) {ball.y = 0};
+
+    ball.x+=90;
+
+    //ballOnPage.style.top = (maxWidth*x/180-10) + "px";
+    //ballOnPage.style.left = (maxHeight*y/180-10) + "px";
+    ballOnPage.style.left = (maxWidth*ball.x/180-10) + "px";
+    ballOnPage.style.top = (maxHeight*ball.y/180-10) + "px";
 }
+//gameinit();
