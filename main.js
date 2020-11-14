f5030ceeb477dccd2a04dc799d668846ea9322f3
Jump3rr <theneonxd@gmail.com>
2020-11-14 14:52:02 +0100

    dodanie dziur, wstępne dodanie funkcji sprawdzania czy kula jest w środku (do poprawy)

--- main.js
+++ main.js
@@ -2,12 +2,30 @@ const ballOnPage = document.querySelector('.ball');
 const board = document.querySelector('.board');
 const maxWidth = board.clientWidth - ballOnPage.clientWidth;
 const maxHeight = board.clientHeight - ballOnPage.clientHeight;
+ballOnPage.id = 'ball';
 
 window.addEventListener('deviceorientation', onDeviceMove)
 
 const ball = {
     x: 0,
     y: 0
+}
+for(let i=0; i<2; i++)
+{
+    const newHole = document.createElement('div');
+    newHole.className ="hole";
+    //newHole.classList.add('holes');
+    newHole.id=i;
+    newHole.setAttribute('name', i);
+    newHole.id = i;
+    newHole.style.top = Math.random()*maxHeight-20 + "px";
+    newHole.style.left = Math.random()*maxWidth-20 + "px";
+    //console.log(a);
+    //let a = newHole.style.top = Math.floor((maxHeight-0)*Math.random())-20 + "px";
+    //newHole.style.left = Math.floor((maxWidth-0)*Math.random())-20 + "px";
+var ref = document.querySelector('script');
+
+ref.parentNode.insertBefore(newHole, ref);
 }
 console.log(maxHeight);
 console.log(maxWidth);
@@ -28,9 +46,18 @@ function onDeviceMove(ev) {
 
     ball.x+=90;
 
-    //ballOnPage.style.top = (maxWidth*x/180-10) + "px";
-    //ballOnPage.style.left = (maxHeight*y/180-10) + "px";
     ballOnPage.style.left = (maxWidth*ball.x/180-10) + "px";
     ballOnPage.style.top = (maxHeight*ball.y/180-10) + "px";
+    console.log(ballOnPage.style.left);
+    check(parseInt(ballOnPage.style.left));
+    
 }
 //gameinit();
+function check(width) {
+    const holeLeft = parseInt(document.getElementById('1').style.left);
+
+    if(width<holeLeft+20 && width>holeLeft-20)
+    {
+        alert("wygrales");
+    }
+}
